fa966975961666452119a2d2980893e6c97854f8
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-03 20:48:15 -0400

    Are You A Student main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+STUDENT_NAME = "student"
+
+students = ["Pedro", "Ailton", "Flavio", "Fedra", "Ellen", "Gisela", "Bruno", "Nuno"]
+
+print("\nAre you a " + STUDENT_NAME + " ? Lets find out...\n")
+
+# user input section (buggy code)
+accept = input("Are you a " + STUDENT_NAME + "?\n(yes/no): ").replace(" ", "")
+if accept.lower() == "yes" or accept.lower() == "y":
+    studentName = input("What is your name?\nName: ").replace(" ", "")
+    # for student in students:
+    #     if studentName.lower() == student.lower():
+    #         print("Welcome to class " + student)
+
+    if studentName in students:
+        print("Welcome to class " + studentName)
+
+    else:
+        print("You're not supposed to be here")
+
+else:
+    print("This service is not for you. Exiting program...")
+    exit()
